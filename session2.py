fe98ebe3d93d989ff7a8104626a876d97fa0f110
gabogara <gabo1gara@gmail.com>
2026-03-18 23:06:58 -0700

    add solution for exercise 3 version 1 session 2

--- session2.py
+++ session2.py
@@ -84,10 +84,49 @@ observed_species1 = "aAAbbbb"
 endangered_species2 = "z"
 observed_species2 = "ZZ"
 
-print(count_endangered_species(endangered_species1, observed_species1))
-print(count_endangered_species(endangered_species2, observed_species2))
+
+# print(count_endangered_species(endangered_species1, observed_species1))
+# print(count_endangered_species(endangered_species2, observed_species2))
 """
-3)
+3)In a wildlife research station, each letter of the alphabet represents a different observation point laid out in a single row. 
+Given a string station_layout of length 26 indicating the layout of these observation points (indexed from 0 to 25), 
+you start your journey at the first observation point (index 0). To make observations in a specific order represented by a string observations, 
+you need to move from one point to another.
+
+The time taken to move from one observation point to another is the absolute difference between their indices, |i - j|.
+
+Write a function that returns the total time it takes to visit all the required observation points in the given order with one movement.
+
+"""
+
+
+def navigate_research_station(station_layout, observations):
+    counter_dict = {}
+
+    for i, val in enumerate(station_layout):
+        counter_dict[val] = i
+
+    current_pos = 0
+    total_time = 0
+
+    for char in observations:
+        next_pos = counter_dict[char]
+        total_time += abs(current_pos - next_pos)
+        current_pos = next_pos
+
+    return total_time
+
+
+station_layout1 = "pqrstuvwxyzabcdefghijklmno"
+observations1 = "wildlife"
+
+station_layout2 = "abcdefghijklmnopqrstuvwxyz"
+observations2 = "cba"
+
+print(navigate_research_station(station_layout1, observations1))
+print(navigate_research_station(station_layout2, observations2))
+"""
+4)
 """
 
 
