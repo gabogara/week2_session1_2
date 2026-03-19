035e2f24bfe147f7f1486e3b03cb28a495a1f9d2
gabogara <gabo1gara@gmail.com>
2026-03-18 19:07:45 -0700

    add solution for exercise 7

--- session1.py
+++ session1.py
@@ -293,9 +293,9 @@ popularity_scores1 = [1, 2, 3, 1, 1, 3]
 popularity_scores2 = [1, 1, 1, 1]
 popularity_scores3 = [1, 2, 3]
 
-print(num_popular_pairs(popularity_scores1))
-print(num_popular_pairs(popularity_scores2))
-print(num_popular_pairs(popularity_scores3))
+# print(num_popular_pairs(popularity_scores1))
+# print(num_popular_pairs(popularity_scores2))
+# print(num_popular_pairs(popularity_scores3))
 
 
 """
@@ -496,5 +496,37 @@ def check_if_complete_transmission(transmission):
 transmission1 = "thequickbrownfoxjumpsoverthelazydog"
 transmission2 = "spacetravel"
 
-print(check_if_complete_transmission(transmission1))
-print(check_if_complete_transmission(transmission2))
+# print(check_if_complete_transmission(transmission1))
+# print(check_if_complete_transmission(transmission2))
+
+
+"""
+7) Ground control is analyzing signal patterns received from different probes. 
+You are given a 0-indexed array signals consisting of distinct strings.
+
+The string signals[i] can be paired with the string signals[j] if the string signals[i] is equal to the reversed string of signals[j]. 
+0 <= i < j < len(signals). Return the maximum number of pairs that can be formed from the array signals.
+
+Note that each string can belong in at most one pair.
+"""
+
+
+def max_number_of_string_pairs(signals):
+    counter = 0
+    seen = set()
+
+    for signal in signals:
+        if signal[::-1] in seen:
+            counter += 1
+        else:
+            seen.add(signal)
+    return counter
+
+
+signals1 = ["cd", "ac", "dc", "ca", "zz"]
+signals2 = ["ab", "ba", "cc"]
+signals3 = ["aa", "ab"]
+
+print(max_number_of_string_pairs(signals1))
+print(max_number_of_string_pairs(signals2))
+print(max_number_of_string_pairs(signals3))
